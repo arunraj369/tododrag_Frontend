2986d90434890f63268e264954a24dac0ca6143b
Arunraj <102640467+arunraj369@users.noreply.github.com>
2024-06-15 16:09:25 +0530

    Update TasksContainer.js

--- client/myapp/src/components/TasksContainer.js
+++ client/myapp/src/components/TasksContainer.js
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faEdit, faComment } from "@fortawesome/free-solid-svg-icons";
+import "../index.css"; 
 
 const TasksContainer = ({ socket }) => {
   const [tasks, setTasks] = useState({});
+  const [editTask, setEditTask] = useState({ id: "", title: "", category: "" });
 
   useEffect(() => {
     function fetchTasks() {
-      fetch("https://tododrag-backend-2.onrender.com/api")
+      fetch("http://localhost:5000/api")
         .then((res) => res.json())
-        .then((data) => setTasks(data)); // intialy data store in state
+        .then((data) => setTasks(data));
     }
     fetchTasks();
   }, []);
 
-  //It listen the sockets updates task
   useEffect(() => {
     socket.on("tasks", (data) => {
       setTasks(data);
@@ -35,26 +38,34 @@ const TasksContainer = ({ socket }) => {
     });
   };
 
+  const handleDelete = (category, id) => {
+    socket.emit("deleteTask", { category, id });
+  };
+
+  const handleEdit = (category, task) => {
+    setEditTask({ id: task.id, title: task.title, category });
+  };
+
+  const handleEditSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("editTask", editTask);
+    setEditTask({ id: "", title: "", category: "" });
+  };
+
   return (
     <div className="container">
-      {/* {console.log("rendering")} */}
-      {/* DragDropContext enables drag and drop functionality within its context. */}
       <DragDropContext onDragEnd={handleDragEnd}>
-        {/* map data from task */}
         {Object.entries(tasks).map((task) => (
           <div
-            className={`${task[1].title.toLowerCase()}__wrapper`} //getting title name from task 1st index
+            className={`${task[1].title.toLowerCase()}__wrapper`}
             key={task[1].title}
           >
             <h3>{task[1].title} Tasks</h3>
             <div className={`${task[1].title.toLowerCase()}__container`}>
-              {/* task storing place is droppable area */}
-              {/* droppableID must unique we have only titles so we use title as droppableId */}
               <Droppable droppableId={task[1].title}>
                 {(provided) => (
                   <div ref={provided.innerRef} {...provided.droppableProps}>
                     {task[1].items.map((item, index) => (
-                      // draggable is where todo content is dislay
                       <Draggable
                         key={item.id}
                         draggableId={item.id}
@@ -67,21 +78,68 @@ const TasksContainer = ({ socket }) => {
                             {...provided.draggableProps}
                             {...provided.dragHandleProps}
                           >
-                            <h4
-                              className={`${task[1].title.toLowerCase()}__items1`}
-                            >
-                              status
-                            </h4>
-                            <p>{item.title}</p>
-                            <p className="comment">
-                              <Link
-                                to={`/comments/${task[1].title}/${item.id}`}
-                              >
-                                {item.comments
-                                  ? `View Comments`
-                                  : "Add Comment"}
-                              </Link>
-                            </p>
+                            {editTask.id === item.id ? (
+                              <form onSubmit={handleEditSubmit}>
+                                <input
+                                  type="text"
+                                  value={editTask.title}
+                                  onChange={(e) =>
+                                    setEditTask((prev) => ({
+                                      ...prev,
+                                      title: e.target.value,
+                                    }))
+                                  }
+                                />
+                                <button type="submit">Save</button>
+                              </form>
+                            ) : (
+                              <>
+                                <h4
+                                  className={`${task[1].title.toLowerCase()}__items1`}
+                                >
+                                  Status
+                                </h4>
+                                <p>{item.title}</p>
+                                <div className="actions">
+                                  <Link
+                                    to={`/comments/${task[1].title}/${item.id}`}
+                                    className="comment-link"
+                                  >
+                                    <FontAwesomeIcon
+                                      icon={faComment}
+                                      className="comment-icon"
+                                    />
+                                    {item.comments
+                                      ? `View Comments`
+                                      : "Add Comment"}
+                                  </Link>
+                                  <span
+                                    className="edit-icon-container"
+                                    onClick={() =>
+                                      handleEdit(task[1].title, item)
+                                    }
+                                  >
+                                    <FontAwesomeIcon
+                                      icon={faEdit}
+                                      className="edit-icon"
+                                    />
+                                    Edit
+                                  </span>
+                                  <span
+                                    className="delete-icon-container"
+                                    onClick={() =>
+                                      handleDelete(task[1].title, item.id)
+                                    }
+                                  >
+                                    <FontAwesomeIcon
+                                      icon={faTrash}
+                                      className="delete-icon"
+                                    />
+                                    Delete
+                                  </span>
+                                </div>
+                              </>
+                            )}
                           </div>
                         )}
                       </Draggable>
